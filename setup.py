ee834f38591c9fc306e7967818346ff33031a85c
Remy Roux <42373104+RemyRoux@users.noreply.github.com>
2018-11-15 17:04:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from shutil import rmtree
 from setuptools import find_packages, setup, Command
 
 # Package meta-data.
-NAME = 'Abaqus_RunINPFiles'
+NAME = 'ABC'
 DESCRIPTION = 'Third-party software to queue Abaqus analyses.'
 URL = 'https://github.com/RemyRoux/Abaqus_RunINPFiles'
 EMAIL = 'me@example.com'
