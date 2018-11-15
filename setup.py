e6ed7b90247c62fcc64c1b68524e556a6b7fc773
Remy Roux <42373104+RemyRoux@users.noreply.github.com>
2018-11-15 17:36:53 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ VERSION = 0.1
 # What packages are required for this module to be executed?
 REQUIRED = [
     # 'requests', 'maya', 'records',
-    'pywin32','RunINPFiles.py'
+    'pywin32','AbaqusRunINPFiles.git'
 ]
 
 # What packages are optional?
